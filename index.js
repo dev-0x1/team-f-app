e03f921de336b0f54048b6b4a3a3f62431389b13
Mukesh Soni <mukesh@grexit.com>
2018-12-20 11:33:00 +0000

    wip: added check for pr

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 module.exports = app => {
   // Your code here
-  app.log('Yay, the app was loaded!')
+    app.log('Yay, the app was loaded!');
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+    app.on('issues.opened', async context => {
+        const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
+        return context.github.issues.createComment(issueComment);
+    });
+    app.on( [
+        'pull_request.opened',
+        'pull_request.edited',
+        'pull_request.synchronize'
+    ], require('./lib/validate-description'));
+
 
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-}
+};
